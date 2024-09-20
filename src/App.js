e0e489acef03e52b26e86942d2ad766394f6205b
GalKalev <132823231+GalKalev@users.noreply.github.com>
2024-09-20 11:32:38 +0300

    homepage and map component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import './App.css';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './screens/HomePage';
-import StatsPage  from './screens/StatsPage';
+import StatsPage from './screens/StatsPage';
 import CompCountriesPage from './screens/CompCountriesPage';
 import FavoritePage from './screens/FavoritePage';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-       <NavBar/>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/compCountries" element={<CompCountriesPage />} />
-      <Route path="/stats" element={<StatsPage />} />
-      <Route path="/favorite" element={<FavoritePage />} />
-    </Routes>
-  </Router>
+ 
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/compCountries" element={<CompCountriesPage />} />
+          <Route path="/stats" element={<StatsPage />} />
+          <Route path="/favorite" element={<FavoritePage />} />
+        </Routes>
+        <Footer />
+      </Router>
+ 
   );
 }
 
